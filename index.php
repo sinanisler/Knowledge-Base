3478f4ccdacaf39e7557dd900072863e79ef9597
sinanisler <sinan@sinanisler.com>
2022-05-25 04:34:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -37,13 +37,17 @@
 </div>
 
 <script>
+    // get the search input
     let search_input = 'test';
+    // put the input and request search
     fetch('<?php bloginfo('home'); ?>/wp-json/wp/v2/search?search=' + search_input)
     .then(response => response.json())
     .then(data => console.log(data));
+    // do something with returned data
 
+    // user device dark mod check
     if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        console.log('dark-mod-on')
+        console.log('dark-mod-on');
     }
 </script>
 
