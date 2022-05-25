761b5647c61d2bb14a84efe1a72a70971a2f0beb
sinanisler <sinan@sinanisler.com>
2022-05-25 05:41:10 +0300

    Update siknowledge.js

--- js/siknowledge.js
+++ js/siknowledge.js
@@ -1,17 +1,6 @@
 
 
 
-// get the search input
-let search_input = 'test'
-
-// put the input and request search
-fetch(bloginfo_url + '/wp-json/wp/v2/search?search=' + search_input)
-.then(response => response.json())
-.then(data => console.log(data));
-
-// do something with returned data
-
-
 
 // user device dark mod check
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -19,5 +8,60 @@ if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').match
 }
 
 
+// get the user search input
+let search_input = 'test'
+
+// put the input and request search
+fetch(bloginfo_url + '/wp-json/wp/v2/search?search=' + search_input)
+.then(response => response.json())
+.then(data => console.log(data));
+
+
+
+
+
+
+
+/*
+
+
+
+fetch(bloginfo_url + '/wp-json/wp/v2/search?search=' + search_input)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+    }
+)
+
+
+render() {
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))}
+        </ul>
+      );
+
+
+*/
 
 
