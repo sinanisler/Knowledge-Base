f2c69de4d0a575f2a568866a88fc34e301a712c5
sinanisler <sinan@sinanisler.com>
2022-05-25 05:14:18 +0300

    header title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title(); ?></title>
+    <title><?php if(is_front_page() || is_home()){ echo get_bloginfo('name');    } else{ echo wp_title('');    }?></title>
     <link rel=icon href="<?php bloginfo("stylesheet_directory"); ?>/img/favicon.png" type="image/png">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.css">
     <script> var bloginfo_url = '<?php bloginfo("url"); ?>'; </script>
